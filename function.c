5ed72b9511c3523b59628334ce53a39f87c5769e
arman-bd <arman@learnfie.com>
2017-03-26 05:36:04 +0600

    Sending Buffer Limit Added

--- function.c
+++ function.c
@@ -1,13 +1,3 @@
-/*
-struct server_config {
-    char default_ip[16];
-    int default_port;
-    char default_dir[512];
-    int compression;
-};
-
-typedef struct server_config s_conf;
-*/
 
 
 int send_header(SOCKET socket, int response, char *c_type, long c_size){
@@ -36,19 +26,43 @@ int send_content(SOCKET socket, char *content){
     return send(socket, content, strlen(content), 0);
 }
 
-int send_file(SOCKET socket, FILE *fp, int file_size){
+int send_file(SOCKET socket, FILE *fp, size_t file_size, size_t max_buffer){
     int byteSent = SOCKET_ERROR;
     char *file_buff;
 
-    file_buff = malloc(file_size + 1);
-    fread(file_buff, sizeof(char), file_size + 1, fp);
-    byteSent = send(socket, file_buff, file_size, 0);
+    if(max_buffer == 0 || max_buffer > file_size){
+        file_buff = malloc(file_size + 1);
+        fread(file_buff, sizeof(char), file_size + 1, fp);
+        byteSent = send(socket, file_buff, file_size, 0);
+    }else{
+        size_t c_position = 0;
+        file_buff = malloc(max_buffer + 1);
+        byteSent = 0;
+        fseek(fp, 0L, SEEK_SET);
+
+        while(c_position + max_buffer <= file_size){
+            fseek(fp, c_position, SEEK_SET);
+            fread(file_buff, sizeof(char), max_buffer + 1, fp);
+            byteSent += send(socket, file_buff, max_buffer, 0);
+            c_position = c_position + max_buffer;
+        }
+        size_t left_buffer = file_size - c_position;
+        if(left_buffer > 0){
+            fseek(fp, c_position, SEEK_SET);
+            fread(file_buff, sizeof(char), left_buffer + 1, fp);
+            if(left_buffer < max_buffer){
+                file_buff[left_buffer] = '\0';
+            }
+            byteSent += send(socket, file_buff, left_buffer, 0);
+        }
+    }
+
     free(file_buff);
     return byteSent;
 }
 
-long getFileSize(FILE *fp){
-    long file_size = 0;
+size_t getFileSize(FILE *fp){
+    size_t file_size = 0;
 
     // Get File Size
     fseek(fp, 0L, SEEK_END);
@@ -57,62 +71,3 @@ long getFileSize(FILE *fp){
     return file_size;
 }
 
-/*
-THIS NEEDS TO BE DONE!!!
-s_conf parse_config(char *file_path){
-
-    FILE *fp;
-    char *conf_array;
-    char *conf_value;
-    char *file_buff;
-    long file_size;
-    int i, j, k;
-
-    s_conf config_store;
-
-
-    if((fp = fopen(file_path, "r")) != NULL){
-        file_size = getFileSize(fp);
-        file_buff = malloc(file_size + 1);
-        fread(file_buff, sizeof(char), file_size + 1, fp);
-
-        conf_array = strtok(file_buff, "\r\n");
-        while(conf_array != NULL){
-            printf(">> %s\n", conf_array);
-
-            if(strstr(conf_array, "#default_ip ") != NULL){
-
-            }
-
-            if(strstr(conf_array, "#default_dir ") != NULL){
-                conf_line = strtok(conf_array, " ");
-                conf_line = strtok(NULL, " ");
-                strcpy(config_store.default_dir, conf_line);
-            }
-            if(strstr(conf_array, "#default_port ") != NULL){
-                conf_line = strtok(conf_array, " ");
-                conf_line = strtok(NULL, " ");
-                config_store.default_port = atoi(conf_line);
-            }
-            if(strstr(conf_array, "#compression ") != NULL){
-                conf_line = strtok(conf_array, " ");
-                conf_line = strtok(NULL, " ");
-                config_store.compression = atoi(conf_line);
-            }
-
-
-            conf_array = strtok(NULL, "\r\n");
-        }
-
-        fclose(fp);
-    }else{
-        printf("Error: Config File Not Found\n");
-        strcpy(config_store.default_ip, "127.0.0.1");
-        config_store.default_port = 8080;
-        strcpy(config_store.default_dir, "htdocs");
-        config_store.compression = 0;
-    }
-
-    return config_store;
-}
-*/
