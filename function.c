fed4f11b4ac7357a4d1e91a379e2773db7e52ac2
arman-bd <arman@learnfie.com>
2017-04-01 04:58:12 +0600

    String Replace and url_decode Function added

--- function.c
+++ function.c
@@ -1,66 +1,5 @@
 
 
-int send_header(SOCKET socket, int response, char *c_type, long c_size){
-    char header[4096];
-
-    // Add Response Code
-    if(response == 200){
-        strcpy(header, "HTTP/1.1 200 OK\n");
-    }
-
-    if(response == 404){
-        strcpy(header, "HTTP/1.1 404 Not Found\n");
-    }
-
-    // Process Header
-    sprintf(header, "%sServer: %s/%s (%s)\n", header, __ServerName, __ServerVersion, __ServerOS);
-    sprintf(header, "%sContent-Type: %s; charset=utf-8\n", header, c_type);
-    sprintf(header, "%sContent-Length: %ld\n", header, c_size);
-    sprintf(header, "%s\n", header);
-
-    // Send Header
-    return send(socket, header, strlen(header), 0);
-}
-
-int send_content(SOCKET socket, char *content){
-    return send(socket, content, strlen(content), 0);
-}
-
-int send_file(SOCKET socket, FILE *fp, size_t file_size, size_t max_buffer){
-    int byteSent = SOCKET_ERROR;
-    char *file_buff;
-
-    if(max_buffer == 0 || max_buffer > file_size){
-        file_buff = malloc(file_size + 1);
-        fread(file_buff, sizeof(char), file_size + 1, fp);
-        byteSent = send(socket, file_buff, file_size, 0);
-    }else{
-        size_t c_position = 0;
-        file_buff = malloc(max_buffer + 1);
-        byteSent = 0;
-        fseek(fp, 0L, SEEK_SET);
-
-        while(c_position + max_buffer <= file_size){
-            fseek(fp, c_position, SEEK_SET);
-            fread(file_buff, sizeof(char), max_buffer + 1, fp);
-            byteSent += send(socket, file_buff, max_buffer, 0);
-            c_position = c_position + max_buffer;
-        }
-        size_t left_buffer = file_size - c_position;
-        if(left_buffer > 0){
-            fseek(fp, c_position, SEEK_SET);
-            fread(file_buff, sizeof(char), left_buffer + 1, fp);
-            if(left_buffer < max_buffer){
-                file_buff[left_buffer] = '\0';
-            }
-            byteSent += send(socket, file_buff, left_buffer, 0);
-        }
-    }
-
-    free(file_buff);
-    return byteSent;
-}
-
 size_t getFileSize(FILE *fp){
     size_t file_size = 0;
 
@@ -70,6 +9,74 @@ size_t getFileSize(FILE *fp){
     fseek(fp, 0L, SEEK_SET);
     return file_size;
 }
+
+/*
+    String Replace Function
+    Source: http://stackoverflow.com/a/32413923
+*/
+
+void str_replace(char *target, const char *needle, const char *replacement){
+    char buffer[1024] = { 0 };
+    char *insert_point = &buffer[0];
+    const char *tmp = target;
+    size_t needle_len = strlen(needle);
+    size_t repl_len = strlen(replacement);
+
+    while (1) {
+        const char *p = strstr(tmp, needle);
+
+        // walked past last occurrence of needle; copy remaining part
+        if (p == NULL) {
+            strcpy(insert_point, tmp);
+            break;
+        }
+
+        // copy part before needle
+        memcpy(insert_point, tmp, p - tmp);
+        insert_point += p - tmp;
+
+        // copy replacement string
+        memcpy(insert_point, replacement, repl_len);
+        insert_point += repl_len;
+
+        // adjust pointers, move on
+        tmp = p + needle_len;
+    }
+
+    // write altered string back to target
+    strcpy(target, buffer);
+}
+
+/*
+    URL Decoding
+    Source: https://www.rosettacode.org/wiki/URL_decoding#C
+*/
+
+int ishex(int x){
+	return	(x >= '0' && x <= '9')	||
+		(x >= 'a' && x <= 'f')	||
+		(x >= 'A' && x <= 'F');
+}
+
+int url_decode(const char *s, char *dec){
+	char *o;
+	const char *end = s + strlen(s);
+	int c;
+
+	for (o = dec; s <= end; o++) {
+		c = *s++;
+		if (c == '+') c = ' ';
+		else if (c == '%' && (	!ishex(*s++)	||
+					!ishex(*s++)	||
+					!sscanf(s - 2, "%2x", &c)))
+			return -1;
+
+		if (dec) *o = c;
+	}
+
+	return o - dec;
+}
+
 
 /*
     Generating RFC 1123 Time Stamp
